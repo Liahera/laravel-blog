bc39d5e7efef14448e298546300518d1daed6445
Liahera <311.lsr@gmail.com>
2020-05-20 21:30:47 +0200

    v2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('email', 128)->unique();
             $table->string('password');
+            $table->tinyInteger('isAdmin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
